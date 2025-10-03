bdaaa51905a6e7106374870ecd2b2e7d3cbd880f
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-10-03 23:15:31 +0600

    Project build and test done

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { corsOptions } from "./config/corsOptions";
 import { AppRoutes } from "./routes";
-import { UUIDService } from "./lib/uuid";
 
 dotenv.config();
 
@@ -36,8 +35,6 @@ app.get("/", async (req, res) => {
 
 // applications routes
 app.use("/api/v1", AppRoutes);
-
-console.log("Husky test", UUIDService.generateCustom(10));
 
 // global error handler
 app.use(globalErrorHandler.globalErrorHandler);
