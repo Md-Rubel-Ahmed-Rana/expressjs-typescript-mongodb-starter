0f19558ac8ed5fc209473435e3215eda2ad9d12c
Md-Rubel-Ahmed-Rana <mdrubelahmedrana521@gmail.com>
2025-10-14 14:56:24 +0600

    Bkash grant token generate service done

--- src/modules/payment/bkash.service.ts
+++ src/modules/payment/bkash.service.ts
@@ -1,4 +1,7 @@
 import { envConfig } from "@/config/index";
+import { HttpStatusCode } from "@/lib/httpStatus";
+import ApiError from "@/middlewares/error";
+import axios, { AxiosRequestConfig } from "axios";
 
 class Service {
   // --- bKash URLs ---
@@ -19,6 +22,53 @@ class Service {
   // --- Session Token (expires hourly) ---
   private id_token: string | null = null;
   private token_expiry: number | null = null;
+
+  // Internal helper to ensure token is valid or refresh it
+  private async ensureToken() {
+    const now = Date.now();
+    if (!this.id_token || !this.token_expiry || now >= this.token_expiry) {
+      await this.grantToken();
+    }
+  }
+
+  //  Grant Token
+  private async grantToken() {
+    try {
+      const config: AxiosRequestConfig = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          username: this.username,
+          password: this.password,
+        },
+      };
+
+      const body = { app_key: this.app_key, app_secret: this.app_secret };
+
+      const { data } = await axios.post(this.grant_token_url, body, config);
+
+      if (!data?.id_token) {
+        throw new ApiError(
+          HttpStatusCode.INTERNAL_SERVER_ERROR,
+          "Failed to get bKash grant token"
+        );
+      }
+
+      this.id_token = data.id_token;
+      // bKash sandbox tokens valid for 3600 seconds (1h)
+      this.token_expiry =
+        Date.now() + (data.expires_in ? data.expires_in * 1000 : 3500 * 1000);
+
+      console.log("bKash grant token received ✅");
+      return data;
+    } catch (error: any) {
+      console.log("Bkash Grant Token Error", error);
+      throw new ApiError(
+        HttpStatusCode.INTERNAL_SERVER_ERROR,
+        `bKash grantToken failed: ${error?.message}`
+      );
+    }
+  }
 }
 
 export const BkashService = new Service();
